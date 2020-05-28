bb528a080fe6d2ac4ebe528dd7d7b7eac2386cb8
Reetok3 <reetok3@gmail.com>
2020-05-28 16:30:54 +0900

    initial commit

---
+++ db/migrate/20200528073009_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.string :logo_pass
+      t.integer :discord_uid
+
+      t.timestamps
+    end
+  end
+end
